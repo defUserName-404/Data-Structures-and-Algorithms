035c3ef235fe10e2e9b4457809d04c8186cb601f
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-23 07:35:15 +0600

    Solved ? Given an integer array, calculate the maximum sum of k consequtive elements

--- Famous-Coding-Interview-Problems/MaxSumOfKElements.hpp
+++ Famous-Coding-Interview-Problems/MaxSumOfKElements.hpp
@@ -2,24 +2,48 @@
 #include <vector>
 
 /*
-! Given an integer array, calculate the maximum sum of k consequtive elements
+? Given an integer array, calculate the maximum sum of k consequtive elements
 */
 
 //* Brute Force Approach
+// int max_sum_of_k_elements(const std::vector<int> &nums, int k)
+// {
+//     int global_max = INT_MIN;
+//     int n = nums.size();
+
+//     for (int i = 0; i < n - k + 1; i++)
+//     {
+//         int current = 0;
+
+//         for (int j = i; j < i + k; j++)
+//             current += nums[j];
+
+//         if (current > global_max)
+//             global_max = current;
+//     }
+
+//     return global_max;
+// }
+
+//* Window Sliding Technique
 int max_sum_of_k_elements(const std::vector<int> &nums, int k)
 {
-    int global_max = INT_MIN;
     int n = nums.size();
 
-    for (int i = 0; i < n - k + 1; i++)
+    // Initializing global max as 0 because it will hold the sum of the first window
+    int global_max = 0;
+    for (int i = 0; i < k; i++)
+        global_max += nums[i];
+
+    // Compute sum of remaining windows one by one
+    // removing first element of previous window and adding a new element
+    int current_max = global_max;
+    for (int i = k; i < n; i++)
     {
-        int current = 0;
+        current_max += nums[i] - nums[i - k];
 
-        for (int j = i; j < i + k; j++)
-            current += nums[j];
-
-        if (current > global_max)
-            global_max = current;
+        if (current_max > global_max)
+            global_max = current_max;
     }
 
     return global_max;
