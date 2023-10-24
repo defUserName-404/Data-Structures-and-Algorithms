59ffc99b9ca60c8a9aed3bbd075570e5ef52e590
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-10-24 14:12:39 +0600

    Interview questions

---
+++ CrackingTheCodingInterview/Chapter8:Recursion&DynamicProgramming/InterviewQuestions.hpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    //! Problem 1: Determine how many ways can a child run up the stairs
+    //! 		   using either 1, 2 or 3 stairs at a time
+    int tripleStep(const int &n);
+};
+
+int Solution::tripleStep(const int &n)
+{
+	
+}
