7f6ef708e8de8b25aed0adf8a368ceff399eb8d1
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2026-01-02 16:17:04 +0600

    feature: add brute force solution for two sum problem

---
+++ interview_prep/array/src/main/java/two_sum/brute_force/Solution.java
@@ -0,0 +1,35 @@
+package two_sum.brute_force;
+
+
+/**
+ * This class implements the solution to the Two Sum problem.
+ * The problem is to find two numbers in an array that add up to a target value.
+ * The brute force approach is used, which is to iterate through all the pairs of elements
+ * in the array and check if their sum equals the target value.
+ *
+ */
+class Solution {
+    /**
+     * Finds two numbers in the given array that add up to the target value.
+     *
+     * @param nums the array of integers
+     * @param target the target value
+     * @return an array containing the indices of the two numbers that add up to the target value
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+
+                    return result;
+                }
+            }
+        }
+
+        return result;
+    }
+}
